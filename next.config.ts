1abbb9af331f4d76420a7ce4e51550f6de92e4fe
sabbanisiddarth <sabbanisiddartha843@gmail.com>
2026-06-02 10:50:51 +0530

    fix: remove basePath to serve app on root domain and fix 404 error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/agrataoutfits",
+  // basePath: "/agrataoutfits",
   images: {
     remotePatterns: [
       {
